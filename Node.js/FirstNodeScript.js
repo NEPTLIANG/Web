074af5374b58908b5e295c988a6f2b41872740e7
NeptLiang <neptliang@outlook.com>
2019-08-26 14:01:57 +0800

    Daily backup

---
+++ Node.js/FirstNodeScript.js
@@ -0,0 +1,6 @@
+var http = require('http');
+var serv = http.createServer(function (req, res) {
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		res.end('<marquee>Smashing Node!</marquee>');
+});
+serv.listen(3000);
