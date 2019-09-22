8dd187525fcbbe1469882618823d2df4a7a1a8ce
NeptLiang <neptliang@outlook.com>
2019-09-22 13:39:33 +0800

    第155场力扣周赛，又一道题都没AC

---
+++ JavaScript/LeetCode20190922q1-UnAc/LeetCode20190922q1-UnAc.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} arr
+ * @return {number[][]}
+ */
+var minimumAbsDifference = function (arr) {
+    arr.sort(cmp);
+    var coupleArray = new Array();
+    for (var row = 0; row < arr.length; row++) {
+        for (var col = row; col < arr.length; col++) {
+            var newCouple = new Couple(arr[row], arr[col]);
+            coupleArray.push(newCouple);
+        }
+    }
+    coupleArray.sort(cmpByDiff);
+    var resultArray = new Array();
+    var couple = new Array(coupleArray[0].left, coupleArray[0].right);
+    resultArray.push(couple);
+    for (var indexOfCouple = 1; coupleArray[indexOfCouple].different == coupleArray[indexOfCouple - 1].different; indexOfCouple++) {
+        var couple = new Array(coupleArray[indexOfCouple].left, coupleArray[indexOfCouple].right);
+        resultArray.push(couple);
+    }
+    console.log(resultArray);
+};
+
+function Couple(left, right) {
+    this.left = left;
+    this.right = right;
+    this.different = right - left;
+}
+
+function cmp(left, right) {  //升序排序
+    return left - right;
+}
+
+function cmpByDiff(couple1, couple2) {  //对元素对按照差的升序排序
+    return couple1.different - couple2.different;
+}
+
+var arr = [4, 2, 1, 3];
+minimumAbsDifference(arr);
