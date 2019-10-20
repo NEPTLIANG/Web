41153b9480655f034696378b056930b0d9074b48
NeptLiang <neptliang@outlook.com>
2019-10-20 15:05:30 +0800

    第159场力扣周赛，还是只AC了一道题；《PHP和MySQL Web开发》好像没有介绍怎么删除数组元素？preg_quote也没见有

--- PHP/LeetCode20191020q2-UnAc/LeetCode21091020q2-UnAc.php
+++ PHP/LeetCode20191020q2-UnAc/LeetCode21091020q2-UnAc.php
@@ -9,10 +9,20 @@ class Solution
      */
     function removeSubfolders($folder)
     {
-        eval($folder);
+//        eval($folder);  //注意参数是字符串数组而不是字符串啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊傻逼
+        for ($indexOfItem = 0; $indexOfItem < sizeof($folder); $indexOfItem++) {  //sizeof返回数组长度
+            for ($indexOfAnotherItem = $indexOfItem + 1; $indexOfAnotherItem < sizeof($folder); $indexOfAnotherItem++) {
+                $p = '/' . preg_quote($folder[$indexOfItem], '/') . '/';  //preg_quote转义字符
+//                echo $p . " " . $folder[$indexOfAnotherItem] . "\n";
+//                var_dump(preg_match($p, $folder[$indexOfAnotherItem]));
+                if (preg_match($p, $folder[$indexOfAnotherItem]) == true) {
+//                    $folder
+                }
+            }
+        }
     }
 }
 
-$folder = ["/a","/a/b","/c/d","/c/d/e","/c/f"];
+$folder = ["/a", "/a/b", "/c/d", "/c/d/e", "/c/f"];
 $a = new Solution();
-//$a->removeSubfolders("folder = [\"/a\",\"/a/b\",\"/c/d\",\"/c/d/e\",\"/c/f\"]");
+$a->removeSubfolders($folder);
