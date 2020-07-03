b0daded8aa26ca38d8c5f951665209a5b6903488
NeptLiang <ming19980628@outlook.com>
2020-07-03 10:18:05 +0800

    基本完成路线管理部分

--- RealTimeBusQuery/Frontend/Route/modify/modify.js
+++ RealTimeBusQuery/Frontend/Route/modify/modify.js
@@ -4,19 +4,17 @@ onload = () => {
     addBtn.addEventListener("click", function () {
         var id = document.getElementById("id").value;
         var name = document.getElementById("name").value;
-        var route = document.getElementById("route").value;
+        var org = document.getElementById("org").value;
         var intro = document.getElementById("intro").value;
         intro = (intro.length > 0) ? intro : "暂无说明";
-        var content = "id=" + id + "&name=" + name + "&route=" + route + "&intro=" + intro;
-        var url = "http://122.51.3.35/device.php";
+        var content = "id=" + id + "&name=" + name + "&org=" + org + "&intro=" + intro;
+        var url = "http://122.51.3.35/route.php";
         if (typeof "XMLHttpRequest" !== "undefined") {
             var xhr = new XMLHttpRequest();
             xhr.onreadystatechange = () => {
                 if (xhr.readyState == 4) {
                     if (xhr.status == "200") {
-                        // var x = eval(xhr.responseText);
                         try {
-                            console.log(xhr.responseText);
                             var response = JSON.parse(xhr.responseText);
                         } catch (e) {
                             alert("没有响应");
@@ -43,9 +41,8 @@ onload = () => {
 function getInfo() {
     console.log(document.location.search.substr(1).split("&"));
     var param = document.location.search.substr(1).split("&");
-    console.log(decodeURIComponent(param[0].substr(param[0].indexOf("=") + 1)))
     document.getElementById("id").value = param[0].substr(param[0].indexOf("=") + 1);
     document.getElementById("name").value = decodeURIComponent(param[1].substr(param[1].indexOf("=") + 1));
-    document.getElementById("route").value = param[2].substr(param[2].indexOf("=") + 1);
+    document.getElementById("org").value = param[2].substr(param[2].indexOf("=") + 1);
     document.getElementById("intro").value = decodeURIComponent(param[3].substr(param[3].indexOf("=") + 1));
 }
