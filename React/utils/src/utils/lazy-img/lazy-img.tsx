f2397fbb5ea9e485f7bd12bc2a1b5529cd2bb9ad
NeptLiang-RCMacBookArchVM <ming19980628@outlook.com>
2023-02-06 16:27:43 +0800

    feat(React/utils/lazy-img): 实现对图片是否已加载过的判断并修复元素指向混乱逻辑错误
    	1. 实现对图片元素是否已加载过的判断；
    	2. 用通过图片元素的 id 或 alt 获取元素来替代原来的遍历所有图片元素，
    	    修复对各个图片元素指向的混乱导致的逻辑错误
    	3. 其他：
    	    * 改用 yarn 与 yarn.lock，依照建议删除 package-lock.json
    	    * 组件属性变量名从 attr 改回惯例 props；
    	    * 代码风格整理
    
    Signed-off-by: NeptLiang-RCMacBookArchVM <ming19980628@outlook.com>

--- React/utils/src/utils/lazy-img/lazy-img.tsx
+++ React/utils/src/utils/lazy-img/lazy-img.tsx
@@ -2,30 +2,31 @@ import React, {
     useEffect
 } from "react";
 
-const LazyImg = attr => {
-    const { conf = {} } = attr;
-    // console.log(attr.size?.height ?? 200);
+interface imgElement { alt: String };
+
+const LazyImg = props => {
+    const { conf = {} } = props;
+    // console.log(props.size?.height ?? 200);
 
     const onImgLoaded = event => {
         // console.log(img.onload);
-        const ele = event.target as HTMLElement;
+        const ele = event.target /* as HTMLElement */;
         if (!ele) { return; } /* setTimeout(() => { */
-        ele.style.opacity = '1';
-        // console.log(ele.parentNode);
+        ele.style.opacity = String(1);      //'1';
+        Object.defineProperty(ele, 'loaded', { value: true });
         if (!ele.parentNode) { return; }
-        (ele.parentNode as HTMLElement).style.background = 'transparent';
+        /* ( */ele.parentNode/*  as HTMLElement) */.style.background = 'transparent';
         //     }
         // }/* , 1000)} */
     }
 
     const loadImg = img => {
         img.onload = onImgLoaded;
-        const src = img.getAttribute('lazy-src') ?? '';
-        img.src = src;
+        img.src = img.getAttribute('lazy-src') ?? '';
     }
 
-    const judgeImgShowed = img => {
-        const { top, bottom, height } = (img.parentNode as HTMLElement)?.getBoundingClientRect()
+    const judgeShowImg = img => {
+        const { top, bottom, height } = /* ( */img.parentNode/*  as HTMLElement) */?.getBoundingClientRect();
         // console.log(
         //     /* window.scrollY, 
         //     img.offsetTop, */ 
@@ -36,39 +37,51 @@ const LazyImg = attr => {
         const windowHeight = window.innerHeight;
         const thresholdHeight = height * (conf.threshold ?? 0);
         const topBaseline = top + thresholdHeight;
+        // console.log('baseline', img.alt, {top, height, conf: conf.threshold})
         const bottomBaseline = bottom - thresholdHeight;
-        // console.log(attr.alt, { return: topBaseline < windowHeight && bottomBaseline > 0 })
+        console.log('judging', img.alt, { return: topBaseline < windowHeight && bottomBaseline > 0 },
+            topBaseline, windowHeight, bottomBaseline)
         return topBaseline < windowHeight && bottomBaseline > 0;
     }
 
     const loadImgConditionally = () => {
-        console.log('conditionally', window.scrollY)
-        const imgList = Array.from(document.getElementsByTagName('img'));
-        imgList.forEach(img => {
-            // console.log(img.alt, { judge: judgeImgShowed(img) });
-            if (!judgeImgShowed(img)) { return; }
-            loadImg(img);
-            // }
-        });
+        // const imgList = Array.from(document.getElementsByTagName('img'));
+        // console.log('conditionally', imgList.map(i => i.alt))
+        // imgList.forEach(img => {
+        const {
+            id = props.alt
+        } = props;
+        const img = (document.getElementById(id) || { alt: '' }) as imgElement;
+        if (
+            Object.getOwnPropertyDescriptor(img, 'loaded')?.value
+                || !judgeShowImg(img)
+        ) { return; }
+        console.log({loaded: Object.getOwnPropertyDescriptor(img, 'loaded')?.value, img: img?.alt});
+        loadImg(img);
+        // });
     };
 
-    console.log('construct');
+    console.log('constructing');
     useEffect(() => {
-        console.log(attr.alt, 'effect');
+        console.log(props.alt, 'effect');
         window.addEventListener('load', () => {
-            console.log('load');
+            console.log('loaded');
             loadImgConditionally();
         });
         window.addEventListener('scroll', () => {
-            console.log('scroll', window.scrollY)
-            loadImgConditionally()
+            console.log('scrolled', window.scrollY)
+            loadImgConditionally();
         });
     });
 
     return (
         <>
-            <div className="util-lazy-img-box" style={attr.style}>
-                <img alt={attr.alt} lazy-src={attr.lazySrc} />
+            <div className="util-lazy-img-box" style={props.style} >
+                <img 
+                    id={props.id ?? props.alt} 
+                    alt={props.alt} 
+                    lazy-src={props.lazySrc} 
+                />
             </div>
 
             <style>{`
