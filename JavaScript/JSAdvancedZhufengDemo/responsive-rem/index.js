90b16b660a4a8f1ba4d9ba08cf961b17d97227aa
NeptLiang <ming19980628@outlook.com>
2024-09-09 23:54:04 +0800

    珠峰JS高级课响应式rem demo
    Signed-off-by: NeptLiang <ming19980628@outlook.com>

---
+++ JavaScript/JSAdvancedZhufengDemo/responsive-rem/index.js
@@ -0,0 +1,17 @@
+const DESIGN_WIDTH = 750;
+const REM_SIZE = 24;
+
+const computeResponsiveRem = () => {
+    const { clientWidth } = document.documentElement;
+    const ratio = clientWidth > DESIGN_WIDTH ?
+        REM_SIZE : clientWidth / DESIGN_WIDTH * REM_SIZE;
+    document.documentElement.style.fontSize = `${ratio}px`;
+};
+
+// 响应式设计——动态计算REM的换算比例
+const makeResponsiveRem = () => {
+    computeResponsiveRem();
+    window.addEventListener('resize', computeResponsiveRem);
+};
+
+export default makeResponsiveRem;
