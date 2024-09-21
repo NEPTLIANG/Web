d65f18b4ac2b366cd81916207bc128cf210fb3a8
NeptLiang <ming19980628@outlook.com>
2024-09-21 13:49:46 +0800

    傻逼小红伞乱杀WSL的lib，还“无法还原”，只好提交并迁移写了一点的题

---
+++ JavaScript/LeetCode/148.排序链表.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=148 lang=javascript
+ *
+ * [148] 排序链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+
+const merge = (array, start, partition, end) => {
+    const result = [];
+    // for (let index = start, length = end - start + 1; index <= end)
+}
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var sortList = function(head) {
+
+};
+// @lc code=end
+
